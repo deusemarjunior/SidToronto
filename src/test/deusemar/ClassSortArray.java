101d5579ded432a1a9cb4a8aad66eb750bf2ca87
Deusemar Bezerra Chaves Junior <djrweb@gmail.com>
2018-09-19 16:15:52 -0300

    Update ClassSortArray.java

--- src/test/deusemar/ClassSortArray.java
+++ src/test/deusemar/ClassSortArray.java
@@ -7,7 +7,28 @@ import java.util.List;
 
 public class ClassSortArray {
 
+	/*
+	2. Write a function that takes array of strings as an argument and returns a new
+	array that contains the same items as the original array, but in the following
+	order: first, all strings that start with a digit (0-9), sorted in descendent
+	order by their numeric value, then all strings that start with anything except
+	the capital letter "P" in alphabetical order, and then all strings that start
+	with the "P" in the order of the string length.
+
+	function sortIt(a) {
+	  // TODO
+	  return a;
+	}
+
+	console.log(sortIt(['001', 'Alpha', 'Gamma', 'Peter', '432', 'Beta', 'Palindrome', '123.4', 'Pit']));
+	// should print ['432', '123.4', '001', 'Alpha', 'Beta', 'Gamma', 'Pit', 'Peter', 'Palindrome']
+
+	
+	/*
+	
 	private static List<String> sortIt(String... strings) {
+		//TODO correct
+		/* wrong
 		List<String> inputArray = Arrays.asList(strings);
 		List<String> outputArray = new ArrayList<String>();
 		
@@ -27,6 +48,7 @@ public class ClassSortArray {
 		}
 		
 		return outputArray;
+		*/
 	}
 	
 	public static void main(String[] args) {
